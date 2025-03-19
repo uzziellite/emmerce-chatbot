ece3b245835e926d5baaa027e67249cb958b983a
Uzziel Lite <uzzielkk@protonmail.com>
2025-03-19 12:48:34 +0300

    Upgrade settings

--- emmerce-chatbot.php
+++ emmerce-chatbot.php
@@ -78,7 +78,8 @@ final class EmmerceChatBot {
                 [
                     'apiUrl' => 'http://your-django-backend.com/api',
                     'wsUrl' => 'ws://your-django-backend.com/ws/chat/',
-                    'position' => esc_attr(get_option('emmerce_chat_position', 'right'))
+                    'position' => esc_attr(get_option('emmerce_chat_position', 'right')),
+                    'accessToken' => esc_attr(get_option('emmerce_access_token'))
                 ]
             );
         }
@@ -145,17 +146,18 @@ final class EmmerceChatBot {
      */
     public static function emmerce_chatbot_register_settings() {
         register_setting('emmerce_chatbot_settings_group', 'emmerce_username');
-        register_setting('emmerce_chatbot_settings_group', 'emmerce_password');
+        register_setting('emmerce_chatbot_settings_group', 'emmerce_hashed_password', [__CLASS__, 'sanitize_password']);
         add_settings_section('emmerce_api_settings', 'API Credentials', [__CLASS__, 'emmerce_api_settings_callback'], 'emmerce-chatbot-settings');
         add_settings_field('emmerce_username', 'Username', [__CLASS__, 'emmerce_username_callback'], 'emmerce-chatbot-settings', 'emmerce_api_settings');
         add_settings_field('emmerce_password', 'Password', [__CLASS__, 'emmerce_password_callback'], 'emmerce-chatbot-settings', 'emmerce_api_settings');
-
         register_setting('emmerce_chatbot_settings_group', 'emmerce_chat_position', 'sanitize_text_field');
         register_setting('emmerce_chatbot_settings_group', 'emmerce_chat_active', 'sanitize_text_field');
 
         add_settings_section('emmerce_widget_settings', 'Chat Widget Settings', [__CLASS__, 'emmerce_widget_settings_callback'], 'emmerce-chatbot-settings');
         add_settings_field('emmerce_chat_position', 'Chat Floater Position', [__CLASS__, 'emmerce_chat_position_callback'], 'emmerce-chatbot-settings', 'emmerce_widget_settings');
         add_settings_field('emmerce_chat_active', 'Activate/Deactivate Chat', [__CLASS__, 'emmerce_chat_active_callback'], 'emmerce-chatbot-settings', 'emmerce_widget_settings');
+
+        add_action('admin_notices', [__CLASS__, 'emmerce_settings_notices']);
     }
 
     /**
@@ -186,8 +188,8 @@ final class EmmerceChatBot {
      * @return void
      */
     public static function emmerce_password_callback() {
-        $password = esc_attr(get_option('emmerce_password'));
-        echo "<input type='password' name='emmerce_password' value='$password' />";
+        echo "<input type='password' name='emmerce_password' value='' />";
+        echo "<p class='description'>Enter password to update it. (For security reasons, this field will remain blank after saving)</p>";
     }
 
     /**
@@ -241,6 +243,58 @@ final class EmmerceChatBot {
             array(),
             '1.0.0'
         );
+    }
+
+    /**
+     * Sanitize password and hash it. Helps to protect the clients' Emmerce
+     * Dashboard by giving the hacker an extra task of cracking this password
+     * in case the WordPress site is breached.
+     *
+     * @since 1.0.0
+     * @param string $password The password to sanitize.
+     * @return string The hashed password.
+     */
+    public static function sanitize_password($password) {
+        if (!empty($password)) {
+            return wp_hash_password($password);
+        }
+        return get_option('emmerce_hashed_password');
+    }
+
+    /**
+     * Settings saved or error notices.
+     *
+     * @since 1.0.0
+     * @return void
+     */
+    public static function emmerce_settings_notices() {
+        if (isset($_GET['settings-updated']) && $_GET['settings-updated']) {
+            if (isset($_GET['emmerce_error']) && $_GET['emmerce_error']) {
+                $error_message = sanitize_text_field(urldecode($_GET['emmerce_error']));
+                echo '<div class="notice notice-error is-dismissible"><p>' . esc_html($error_message) . '</p></div>';
+            } else {
+                echo '<div class="notice notice-success is-dismissible"><p>Settings successfully updated.</p></div>';
+            }
+        }
+    }
+
+    /**
+     * Get the access token for making subsequent API connections
+     * @since 1.0.0
+     * @return string
+     */
+    public static function get_access_token() {
+        $username = get_option('emmerce_username');
+        $hashed_password = get_option('emmerce_hashed_password');
+        $password = $_POST['emmerce_password'];
+        if (wp_check_password($password, $hashed_password)) {
+            //your code here.
+        } else {
+            //your code here.
+            $error_message = urlencode("Incorrect Password");
+            wp_redirect(add_query_arg('emmerce_error', $error_message, admin_url('admin.php?page=emmerce-chatbot-settings')));
+            exit;
+        }
     }
 
 }
